ace269361cde6eb7424b6bc1cba4fdae25be7adc
Влад <>
2020-10-01 20:17:16 +0300

    Initial commit

---
+++ Lesson10(1)Tests/Lesson10_1_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Lesson10_1_Tests.swift
+//  Lesson10(1)Tests
+//
+//  Created by Влад on 9/29/20.
+//
+
+import XCTest
+@testable import Lesson10_1_
+
+class Lesson10_1_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
